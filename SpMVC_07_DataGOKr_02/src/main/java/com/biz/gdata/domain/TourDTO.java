585cbc3beacde480558696c54943a4ef30068f7d
leeiter <jesgipne@hanmail.net>
2019-12-10 12:18:53 +0900

    2019-12-10

---
+++ SpMVC_07_DataGOKr_02/src/main/java/com/biz/gdata/domain/TourDTO.java
@@ -0,0 +1,27 @@
+package com.biz.gdata.domain;
+
+public class TourDTO {
+	
+	public String t_city;
+
+	
+	public TourDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public TourDTO(String t_city) {
+		super();
+		this.t_city = t_city;
+	}
+
+	public String getT_city() {
+		return t_city;
+	}
+
+	public void setT_city(String t_city) {
+		this.t_city = t_city;
+	}
+
+
+}
